be8739b220531749328a8e2ad717edf09c01c583
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-07-26 21:12:00 +0800

    修正品牌页面模块引用路径

--- src/pages/yewurules/modules/material/BrandView.js
+++ src/pages/yewurules/modules/material/BrandView.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Plus, CheckCircle, XCircle, Download, Edit, Upload } from 'lucide-react';
 import { Button, Input, Select, Modal, Table, Radio } from 'antd';
-import StatusTag from '../../../components/StatusTag';
-import QueryBar, { QueryItem } from '../../../components/QueryBar';
+import StatusTag from '../../../../components/StatusTag';
+import QueryBar, { QueryItem } from '../../../../components/QueryBar';
 import {
   mockBrandListData,
-} from '../../../mock/businessRulesMock';
+} from '../../../../mock/businessRulesMock';
 
 const BrandView = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
