3cd2d23ef43648de1c19efc2c78c79d904c4834c
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-07-26 22:49:39 +0800

    新增权限模块出口

---
+++ src/pages/yewurules/modules/permission/index.js
@@ -0,0 +1,5 @@
+export { CompanyPlateAssetAuthView } from './PermissionAuthViews';
+export { NODeviceAssetAuthView } from './PermissionAuthViews';
+export { CompanyBelongingAuthView } from './PermissionAuthViews';
+export { ReceiptRuleManagementView } from './PermissionRuleViews';
+export { AssetAllocationRuleView } from './PermissionRuleViews';
