701e4723273df873dd9d8b48bba225dbdd79c009
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-07-27 16:47:00 +0800

    feat: support viewing applicant assets

--- src/pages/employeeSelfService/ApplicantInfoCard.js
+++ src/pages/employeeSelfService/ApplicantInfoCard.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Card, Descriptions, Tag } from 'antd';
+import { Eye } from 'lucide-react';
+import { Button, Card, Descriptions, Space, Tag } from 'antd';
 
-export default function ApplicantInfoCard({ applicant, applyDate }) {
+export default function ApplicantInfoCard({ applicant, applyDate, onViewAssets }) {
   return (
     <Card title="申请人信息" size="small">
       <Descriptions bordered size="small" column={3}>
-        <Descriptions.Item label="申请人">{applicant.id}-{applicant.name}</Descriptions.Item>
+        <Descriptions.Item label="申请人">
+          <Space size={8}>
+            <span>{applicant.id}-{applicant.name}</span>
+            {onViewAssets && (
+              <Button
+                type="link"
+                size="small"
+                icon={<Eye size={14} />}
+                className="px-0"
+                onClick={onViewAssets}
+              >
+                查看名下资产
+              </Button>
+            )}
+          </Space>
+        </Descriptions.Item>
         <Descriptions.Item label="联系电话">{applicant.phone}</Descriptions.Item>
         <Descriptions.Item label="邮箱">{applicant.email}</Descriptions.Item>
         <Descriptions.Item label="公司">{applicant.company}</Descriptions.Item>
