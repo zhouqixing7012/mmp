59765aa340f6471d7bbd1cc531a3d106679f8685
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-08-03 02:49:17 +0800

    feat: export asset replacement pages

---
+++ src/pages/assetReplacement/index.js
@@ -0,0 +1,6 @@
+export { default as ReplacementAssetsPage } from './ReplacementAssetsPage';
+export { default as ReplacementApplyPage } from './ReplacementApplyPage';
+export { default as MyReplacementApplicationsPage } from './MyReplacementApplicationsPage';
+export { default as ReplacementMisPage } from './ReplacementMisPage';
+export { default as ReplacementHandlingPage } from './ReplacementHandlingPage';
+export { default as ReplacementConfirmPage } from './ReplacementConfirmPage';
