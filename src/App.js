cba244415aec815238ec885519d70f55799891fb
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-08-22 17:28:30 +0800

    localize Ant Design date components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import routes from './config/routes';
@@ -6,15 +8,17 @@ import PrototypeAnnotationLayer from './prototype-annotations/PrototypeAnnotatio
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {routes.map((route) => {
-          const Component = route.Page;
-          return Component ? <Route key={route.path} path={route.path} element={<Component />} /> : null;
-        })}
-      </Routes>
-      <PrototypeAnnotationLayer />
-    </BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {routes.map((route) => {
+            const Component = route.Page;
+            return Component ? <Route key={route.path} path={route.path} element={<Component />} /> : null;
+          })}
+        </Routes>
+        <PrototypeAnnotationLayer />
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
