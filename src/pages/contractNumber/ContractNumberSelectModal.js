f3a238b672b66409f5daf7f18145285c10663254
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-09-11 10:23:10 +0800

    Add remark column to contract number selector

--- src/pages/contractNumber/ContractNumberSelectModal.js
+++ src/pages/contractNumber/ContractNumberSelectModal.js
@@ -33,13 +33,14 @@ export default function ContractNumberSelectModal({
     { title: '标签号', dataIndex: 'assetTag', width: 160 },
     { title: '合约号码', dataIndex: 'phoneNumber', width: 190 },
     { title: '套餐金额', dataIndex: 'packageName', width: 180 },
+    { title: '备注', dataIndex: 'remark', width: 220, render: (value) => value || '-' },
   ];
 
   return (
     <Modal
       title="选择合约号码"
       open={open}
-      width={820}
+      width={960}
       destroyOnHidden
       onCancel={onCancel}
       footer={(
