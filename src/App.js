47d7bd21c7f23d91bc58e886cde6c985745ba16b
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-09-12 14:55:34 +0800

    feat: animate all route page entries

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import routes from './config/routes';
 import PrototypeAnnotationLayer from './prototype-annotations/PrototypeAnnotationLayer';
+
+function AppRoutes() {
+  const location = useLocation();
+
+  return (
+    <div key={location.key} className="mmp-route-motion">
+      <Routes location={location}>
+        {routes.map((route) => {
+          const Component = route.Page;
+          return Component ? <Route key={route.path} path={route.path} element={<Component />} /> : null;
+        })}
+      </Routes>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <ConfigProvider locale={zhCN}>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          {routes.map((route) => {
-            const Component = route.Page;
-            return Component ? <Route key={route.path} path={route.path} element={<Component />} /> : null;
-          })}
-        </Routes>
+        <AppRoutes />
         <PrototypeAnnotationLayer />
       </BrowserRouter>
     </ConfigProvider>
